fcaa320c300eab12d367a05d12bf516a230baec5
GeneralWar <generalwar@outlook.com>
2019-11-26 12:51:28 +0800

    新增对js-binding的追踪

---
+++ frameworks/cocos2d-x/cocos/scripting/js-bindings/custom/Custom.h
@@ -0,0 +1,10 @@
+//
+//  Custom.h
+//  cocos2d_js_bindings
+//
+//  Created by 朱嘉灵 on 2019/11/26.
+//
+
+#pragma once
+
+void log_to_nslogger(const char* message);
---
+++ frameworks/cocos2d-x/cocos/scripting/js-bindings/custom/Custom.m
@@ -0,0 +1,14 @@
+//
+//  Custom.m
+//  libjscocos2d Mac
+//
+//  Created by 朱嘉灵 on 2019/11/26.
+//
+
+#include "Custom.h"
+#import "NSLogger.h"
+
+void log_to_nslogger(const char* message)
+{
+    NSLog(0, 0, 0, 0, 0, message);
+}
